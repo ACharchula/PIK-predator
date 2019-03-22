bcb66895b20e256e949d86e6aa00f1708ae981c3
tokerson <kuba.tokarzewski@gmail.com>
2019-03-22 23:05:59 +0100

    added react test with enzyme

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+configure({adapter: new Adapter()});
+
+describe('App', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <App />
+    );
+  });
+
+  test('should be right name', () => {
+    expect(
+      wrapper.contains(<p>IT is test</p>)
+    ).toBe(true);
+
+  })
+
+})
+
