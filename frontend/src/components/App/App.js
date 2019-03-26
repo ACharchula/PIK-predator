5bdc8fdc1a188a8a73720518e07c6777ab7523b0
tokerson <kuba.tokarzewski@gmail.com>
2019-03-26 21:34:04 +0100

    added setupTests.js

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -22,7 +21,6 @@ class App extends Component {
         return (
             <div className="App">
                 <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
                     <h1 className="App-title">{this.state.message}</h1>
                 </header>
                 <p>IT is test</p>
--- frontend/src/components/App/App.test.js
+++ frontend/src/components/App/App.test.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
-import Adapter from 'enzyme-adapter-react-16';
-import { shallow, configure } from 'enzyme';
+import { shallow } from 'enzyme';
 
-configure({adapter: new Adapter()});
 
 describe('App', () => {
   let wrapper;
