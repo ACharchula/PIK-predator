dc44c8ec1ebdd3d5ce002128df5e391645023974
tokerson <kuba.tokarzewski@gmail.com>
2019-03-27 21:33:55 +0100

    #21 deleted unused commits

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import './App.scss';
 import axios from 'axios';
-
 import Container from 'react-bootstrap/Container'
 import Layout from '../Layout/Layout'
 
