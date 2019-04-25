ea4d6e7e70f5fc64465a90621fc3db2b2d57b4ed
dwiszyns <dominikwisza@gmail.com>
2019-04-25 23:16:55 +0200

    #49 added tests to LoginDropdown and Home

--- frontend/src/components/Header/LoginDropdown/LoginDropdown.test.js
+++ frontend/src/components/Header/LoginDropdown/LoginDropdown.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import LoginDropdown from './LoginDropdown';
-import { mount } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import configureStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
 
@@ -9,15 +9,27 @@ import { faUser } from '@fortawesome/free-solid-svg-icons'
 
 library.add(faUser);
 
+class mockedAuth{
+    getLogin = () => {};
+    login = () => {};
+    logout = () => {};
+}
+
+
 describe('LoginDropdown', () => {
     const initialState = { login:{login_status: "logged_out"}};
     const mockStore = configureStore();
     let store, wrapper;
+    const mAuth = new mockedAuth();
 
     beforeEach(() => {
         store = mockStore(initialState);
-        wrapper = mount(<Provider store={store}><LoginDropdown /></Provider>);
+        wrapper = mount(<Provider store={store}><LoginDropdown auth={mAuth} /></Provider>);
     });
+
+    afterEach(()=>{
+        wrapper.unmount();
+    })
 
     it('renders loginDropdown', ()=>{
         expect(wrapper.length).toEqual(1);
@@ -27,10 +39,27 @@ describe('LoginDropdown', () => {
         expect(wrapper.find('.loginDrop').length).toEqual(1);
     });
 
-    it('renders log in button when logged out', () => {
-        if(store.getState().login.login_status === "logged_out") {
-            expect(wrapper.findWhere(x => x.text() === "Log in").find('button').length).toEqual(1);
-        }
+    it('Finds button login', ()=> {
+        console.log(wrapper.debug());
+        expect(wrapper.findWhere(x => x.text() === "Log in").find('button').length).toEqual(1);
+        expect(wrapper.find("button").length).toEqual(1);
     });
 
-});
+    it('Changes state to logged_in, finds logout button', ()=> {
+        console.log(mAuth);
+        wrapper.unmount();
+        store = mockStore({login:{login_status: "logged_in"}});
+        wrapper = mount(<Provider store={store}><LoginDropdown LoginDropdown auth={mAuth}/></Provider>);
+        expect(wrapper.find("button").length).toEqual(2);
+        expect(wrapper.findWhere(x => x.text() === "Log out").find('button').length).toEqual(1);
+    });
+
+    it('renders log in button when logged out', () =>
+    {
+        if(store.getState().login.login_status === "logged_out")
+        { expect(wrapper.findWhere(x => x.text() === "Log in").find('button').length).toEqual(1); }
+    });
+
+
+
+})
--- frontend/src/components/Home/Home.test.js
+++ frontend/src/components/Home/Home.test.js
@@ -1,29 +1,40 @@
 import React from 'react';
 import Home from './Home';
 import { mount } from 'enzyme';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBars, faShoppingCart, faUser, faSearch } from '@fortawesome/free-solid-svg-icons'
+import loading from "../Callback/loading.svg";
+import LoginDropdown from "../Header/LoginDropdown/LoginDropdown.test";
+
+library.add(faBars, faShoppingCart, faUser, faSearch);
+
 
 
 describe('Home', () => {
+    const initialState = { products:[{
+            "name": "Asus Vivobook",
+            "url": "https://www.asus.com/websites/global/products/5hcFTTB98JtqhrE6/img/common/response/asus-vivobook.png",
+            "cpu": "Intel core i5-8250U"
+    }]};
     let wrapper;
 
-    /*beforeEach(() => {
-        wrapper = mount(
-            <Home/>
-        );
-    });*/
+    beforeEach(() => {
+        wrapper = mount(<Home />);
+    });
 
-    test('callingProducts', () => {
-        /*expect(
-            getProducts
+    test('Products are set properly', () => {
+        wrapper.setState(initialState);
+        expect(wrapper.length).toBe(1);
+        expect(wrapper.contains(initialState)).toBe(true);
+    });
 
-        ).toHaveBeenCalled();*/
-        expect(true).toBe(true);
-    })
+    afterEach(() => {
+        wrapper.unmount();
+    });
 
-    /*afterEach(() => {
-        wrapper = umount(
-            <Home/>
-        );
-    });*/
+} );
 
-})
