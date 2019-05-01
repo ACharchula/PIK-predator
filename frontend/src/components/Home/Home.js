b9b1a54c4aeef72c695346d31ed613161d503dc4
dwiszyns <dominikwisza@gmail.com>
2019-05-01 22:04:00 +0200

    #66 Now we retrieve data from database

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import JSON from '../../products.json';
+import axios from 'axios';
 import Product from '../Product/Product';
 
 import './Home.scss';
@@ -15,6 +16,28 @@ class Home extends Component {
     }
 
     getProducts = () => {
+        axios.get('http://localhost:8080/catalog/all')
+            .then(response=> {
+                if (response.data.message) {
+                    console.log(
+                        `Got ${Object.entries(response.data.message).length} breeds`
+                    )
+                }
+                console.log(response.data)
+                const notebooks = response.data;
+                console.log(notebooks)
+                this.setState({products: notebooks});
+
+                }
+
+            )
+        axios.get("http://localhost:8080/api/hello")
+            .then(response =>{
+                console.log(
+                    response.data
+                )
+            })
+        console.log(this.state.products)
         return this.state.products.map((product, i) => {
             return (
                     <Product product={product} key={i}  />
