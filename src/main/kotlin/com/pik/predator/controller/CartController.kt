34e8368cea523940ac15239a3847803610d16517
Radek Panuszewski <radoslaw.panuszewski15@gmail.com>
2019-05-20 19:48:18 +0200

    #82 Add clear cart endpoint, refactor

--- src/main/kotlin/com/pik/predator/controller/CartController.kt
+++ src/main/kotlin/com/pik/predator/controller/CartController.kt
@@ -4,6 +4,7 @@ import com.pik.predator.db.data.BasicProductInfo
 import com.pik.predator.db.data.Cart
 import com.pik.predator.db.data.mapToBasicInfo
 import com.pik.predator.db.repository.CartRepository
+import com.pik.predator.db.repository.OrderRepository
 import com.pik.predator.db.repository.ProductRepository
 import com.pik.predator.helpers.*
 import com.pik.predator.service.SequenceGenerator
@@ -15,56 +16,79 @@ import javax.servlet.http.HttpServletResponse
 class CartController(
     private val cartRepository: CartRepository,
     private val productRepository: ProductRepository,
+    private val orderRepository: OrderRepository,
     private val sequenceGenerator: SequenceGenerator
 ) {
-
     /**
      * Returns all products in the cart
      * @param userId id of the user who owns this cart
      * @return list of basic infos about products in cart
      */
-    //TODO change to /carts/{cartId}, cart should be created (with location returned) before calling that method
-    @GetMapping("/carts/{userId}")
+    @GetMapping("/users/{userId}/cart")
     @CrossOrigin
     fun getProductsInCart(@PathVariable userId: Int, response: HttpServletResponse): List<BasicProductInfo>? {
-        return cartRepository.findByUserId(userId)?.items?.mapToBasicInfo()
-            .also { items ->
-                if (items != null) response.ok()
-                else response.notFound()
-            }
+        return cartRepository.findByUserId(userId)?.items
+            .alsoNullable(
+                onNotNull = { response.ok() },
+                onNull = { response.notFound() }
+            )
     }
 
     /**
      * Adds product to the cart
      * @param userId id of the user who owns this cart
-     * @param productId id of the product to be added (given in request body)
+     * @param productIds list of ids of the products to be added (given in request body)
      */
-    //TODO change to /carts/{cartId}, cart should be created (with location returned) before calling that method
-    @PostMapping("/carts/{userId}")
+    @PostMapping("/users/{userId}/cart")
     @CrossOrigin
-    fun addProductToCart(@PathVariable userId: Int, @RequestParam productId: Int, response: HttpServletResponse) {
-        val cart: Cart = cartRepository.findByUserId(userId)
-            .let { cart ->
-                if (cart != null) cart.also { response.ok() }
-                else createCart(userId).also { response.created() }
-            }
+    fun addProductToCart(@PathVariable userId: Int, @RequestBody productIds: List<Int>, response: HttpServletResponse) {
+        val cart: Cart =
+            cartRepository.findByUserId(userId)
+                .letNullable(
+                    onNotNull = { cart -> response.ok(); cart },
+                    onNull = { response.created(); createCart(userId) }
+                )
 
-        productRepository.getById(productId)
-            ?.let { product ->
-                cart.items += product
-                cartRepository.save(cart)
-            }
+        for (productId in productIds) {
+            productRepository.getById(productId)
+                ?.let { product ->
+                    cart.items += product.mapToBasicInfo()
+                    cartRepository.save(cart)
+                }
+        }
+    }
+
+    /**
+     * Deletes all contents from the cart
+     * @param userId id of the user who owns this cart
+     */
+    @DeleteMapping("/users/{userId}/cart")
+    @CrossOrigin
+    fun clearCart(@PathVariable userId: Int, response: HttpServletResponse) {
+        cartRepository.findByUserId(userId)
+            .letNullable(
+                onNotNull = { cart ->
+                    cartRepository.delete(cart)
+                    response.ok()
+                },
+                onNull = { response.notFound() }
+            )
     }
 
     /**
      * Creates order from this cart's contents
      * @param userId id of the user who owns this cart
      */
-    //TODO change URL to something like /checkout/{cartId}
     @PostMapping("/carts/{userId}/checkout")
     @CrossOrigin
-    fun checkout(@PathVariable("userId") userId: Int) {
-
+    fun checkout(@PathVariable("userId") userId: Int, response: HttpServletResponse) {
+        cartRepository.findByUserId(userId)
+            .letNullable(
+                onNotNull = { cart ->
+                    //TODO create repository and return its location in response
+                },
+                onNull = { response.notFound() }
+            )
     }
 
     private fun createCart(userId: Int): Cart =
