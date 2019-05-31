b737a570dc0d87e683416322cfc5bf3f23645748
semidude <radoslaw.panuszewski15@gmail.com>
2019-06-01 00:33:33 +0200

    Add authorization tests

--- src/test/kotlin/com/pik/predator/controller/CartControllerTest.kt
+++ src/test/kotlin/com/pik/predator/controller/CartControllerTest.kt
@@ -18,9 +18,21 @@ import org.mockito.Mock
 import org.springframework.test.context.junit4.SpringRunner
 import org.junit.Assert.*
 import org.mockito.Spy
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
 import java.util.*
 import javax.servlet.http.HttpServletResponse
 import javax.servlet.http.HttpServletResponse.*
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 
 @RunWith(SpringRunner::class)
 class CartControllerTest {
