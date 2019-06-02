7f6f48a5fe94a0dcd232e63513b869164eac03c7
dwiszyns <dominikwisza@gmail.com>
2019-06-02 23:32:44 +0200

    Fixed tests

--- frontend/src/components/Home/Home.test.js
+++ frontend/src/components/Home/Home.test.js
@@ -1,35 +1,42 @@
 import React from 'react';
 import Home from './Home';
 import { mount } from 'enzyme';
-import { MemoryRouter} from 'react-router-dom';
-
+import {BrowserRouter, MemoryRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faBars, faShoppingCart, faUser, faSearch } from '@fortawesome/free-solid-svg-icons'
+import configureStore from "redux-mock-store";
 
 library.add(faBars, faShoppingCart, faUser, faSearch);
 
 
 
 describe('Home', () => {
-    const initialState = { products:[{
+    const initialState = {
+        products:[{
             "manufacturer": "Asus",
             "model": "Vivobook",
             "imageUrl": "https://www.asus.com/websites/global/products/5hcFTTB98JtqhrE6/img/common/response/asus-vivobook.png",
             "processor": "Intel core i5-8250U"
-        }]};
-    let wrapper;
+        }],
+    filter:{filters:[]}};
+    const mockStore = configureStore();
+    let wrapper,store;
 
     beforeEach(() => {
+        store=mockStore(initialState);
         wrapper = mount(
+            <Provider store={store}>
             <MemoryRouter>
                 <Home />
-            </MemoryRouter>);
+            </MemoryRouter>
+            </Provider>);
     });
 
     test('Products are set properly', () => {
         wrapper.setState(initialState);
         expect(wrapper.length).toBe(1);
-        expect(wrapper.state()).toEqual(initialState);
+        expect(wrapper.state().filter.filters).toEqual([]);
     });
 
     afterEach(() => {
